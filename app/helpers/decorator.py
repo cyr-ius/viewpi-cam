83b0c9a6b7486f5cd57e966d92eb27d20dfb28dd
cyr-ius <cyr-ius@ipocus.net>
2023-08-19 18:30:59 +0200

    Up

--- app/helpers/decorator.py
+++ app/helpers/decorator.py
@@ -5,11 +5,19 @@ from flask import redirect, url_for, request, g, current_app
 def auth_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
+        if g.user is None and request.args.get("token") is None:
+            return redirect(url_for("auth.login", next=request.url))
+        return f(*args, **kwargs)
+
+    return decorator
+
+
+def token_accept(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
         if (argtoken := request.args.get("token")) is not None:
             if argtoken != current_app.settings.get("token"):
                 return redirect(url_for("auth.login", next=request.url))
-        elif g.user is None:
-            return redirect(url_for("auth.login", next=request.url))
         return f(*args, **kwargs)
 
     return decorator
