a9dc9c4f2addde0e5cdc6f9e729c4588d4529938
cyr-ius <cyr-ius@ipocus.net>
2023-08-20 17:33:56 +0200

    Remove unsed code

--- dockerfiles/docker-entrypoint.sh
+++ dockerfiles/docker-entrypoint.sh
@@ -1,24 +1,6 @@
 #!/bin/sh
-# echo /opt/vc/lib > /etc/ld.so.conf
-# ldconfig
-
-# Raspimjpeg
-# cp /bin/raspimjpeg /opt/vc/bin/
-# chmod 755 /opt/vc/bin/raspimjpeg
-# if [ ! -e /usr/bin/raspimjpeg ]; then
-#    ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
-# fi
-
-# Static folder
-cp -Rv ./app/ressources/css/fonts /app/static/css
-cp -Rv ./app/ressources/img /app/static
 
 # Assets
 flask assets build
 
-#Run scheduler
-# flask scheduler >/dev/null 2&>1 &
-
 exec gunicorn --bind 0.0.0.0:8000 --workers 1 --worker-class gthread --threads 4 'app:create_app()' "$@"
-
-
