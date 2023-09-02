5a48f565c3e4987188fc3b1b7d8740bd5a4ff077
cyr-ius <cyr-ius@ipocus.net>
2023-09-02 18:41:23 +0200

    Add mode

--- app/helpers/raspiconfig.py
+++ app/helpers/raspiconfig.py
@@ -77,9 +77,9 @@ class RaspiConfig:
         if os.path.isfile(self.bin):
             # Create FIFO
             if not os.path.isfile(self.control_file):
-                os.mkfifo(self.control_file)
+                os.mkfifo(self.control_file, mode="0o600")
             if not os.path.isfile(self.motion_pipe):
-                os.mkfifo(self.motion_pipe)
+                os.mkfifo(self.motion_pipe, mode="0o600")
             # Execute binary
             Popen(self.bin)
             self.logging.info("Start raspimjpeg")
