0bdf0d21199745e3a95f82d56ccff5f3a5ed7cee
cyr-ius <cyr-ius@ipocus.net>
2023-09-03 18:41:02 +0200

    Add link media

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Assets
 flask assets build
 
-#
-ln -s /app/media /app/static/media
+# Add link media to static folder
+ln -s /app/media/ /app/static/
 
 exec gunicorn --bind 0.0.0.0:8000 --workers 1 --worker-class gthread --threads 4 'app:create_app()' "$@"
