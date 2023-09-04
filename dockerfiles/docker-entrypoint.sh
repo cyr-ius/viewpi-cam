20ac1da4b9331f9dc1fb53836e7a7226c5dfec42
cyr-ius <cyr-ius@ipocus.net>
2023-09-04 08:38:26 +0200

    Fix docker-endpoint

--- dockerfiles/docker-entrypoint.sh
+++ dockerfiles/docker-entrypoint.sh
@@ -3,4 +3,7 @@
 # Assets
 flask assets build
 
+# Add link media to static folder
+ln -s /app/media/ /app/static/
+
 exec gunicorn --bind 0.0.0.0:8000 --workers 1 --worker-class gthread --threads 4 'app:create_app()' "$@"
