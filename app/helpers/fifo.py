e794a7e7616b0ef739c476f7a3860cef9d5e8a10
cyr-ius <cyr-ius@ipocus.net>
2023-10-01 12:06:25 +0200

    Add fifo helpers

---
+++ app/helpers/fifo.py
@@ -0,0 +1,42 @@
+"""Fifo helper."""
+import os
+
+from flask import current_app as ca
+
+from .utils import write_log, execute_cmd
+
+
+def open_pipe(pipename: str):
+    if not os.path.exists(pipename):
+        write_log(f"Making Pipe to receive capture commands {pipename}")
+        execute_cmd(f"mkfifo {pipename}")
+        execute_cmd(f"chmod 666 {pipename}")
+    else:
+        write_log(f"Capture Pipe already exists ({pipename})")
+
+    try:
+        pipe = os.open(pipename, os.O_RDONLY | os.O_NONBLOCK)
+        return pipe
+    except OSError as error:
+        write_log(f"Error open pipe {pipename} {str(error)}")
+
+
+def send_motion(cmd: str) -> None:
+    """Send command to pipe."""
+    try:
+        pipe = os.open(ca.raspiconfig.motion_pipe, os.O_WRONLY | os.O_NONBLOCK)
+        os.write(pipe, f"{cmd}\n".encode("utf-8"))
+        os.close(pipe)
+        write_log(f"Motion - Send {cmd}")
+    except Exception as error:  # pylint: disable=W0718
+        write_log(f"Motion - {error}")
+
+
+def check_motion(pipe):
+    if isinstance(pipe, bool):
+        return ""
+    try:
+        ret = os.read(pipe, 1).decode("utf-8").replace("\n", "")
+    except Exception:  # pylint: disable=W0718
+        ret = ""
+    return ret
