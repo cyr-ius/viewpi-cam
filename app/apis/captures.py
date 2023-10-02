cf36d607d9ba8c90e50c916ba68c501aa213c458
Cyr-ius <1258123+cyr-ius@users.noreply.github.com>
2023-10-02 21:59:45 +0200

    Update captures.py

--- app/apis/captures.py
+++ app/apis/captures.py
@@ -1,36 +1,35 @@
 """Api camera."""
-from flask import current_app as ca
+from flask import current_app as ca, request
 from flask_restx import Namespace, Resource, fields
 
 from ..helpers.decorator import token_required
+from ..helpers.raspiconfig import RaspiConfigError
 from .models import message, forbidden
 
 api = Namespace("captures")
 api.add_model("Error", message)
 api.add_model("Forbidden", forbidden)
 
-action_m = api.model(
-    "Action",
-    {"action": fields.Boolean(required=True, description="Stop or Start action")},
-)
-
 
 @api.response(204, "Action execute")
 @api.response(422, "Error", message)
 @api.response(403, "Forbidden", forbidden)
-@api.route("/captures/video")
+@api.route("/captures/video/start", endpoint="captures_video_start")
+@api.route("/captures/video/stop", endpoint="captures_video_stop")
 class Camera(Resource):
     """Camera."""
 
     @token_required
-    @api.expect(action_m)
     def post(self):
         """Get capture video."""
-        if api.payload.get("action") == "start":
-            ca.raspiconfig.send("cam 1")
-        if api.payload.get("action") == "stop":
-            ca.raspiconfig.send("cam 0")
-        return "", 204
+        try:
+            if request.endpoint == "api.captures_video_start":
+                ca.raspiconfig.send("cam 1")
+            if request.endpoint == "api.captures_video_stop":
+                ca.raspiconfig.send("cam 0")
+            return "", 204
+        except RaspiConfigError as error:
+            abort(422, error)
 
 
 @api.response(204, "Action execute")
@@ -43,23 +42,30 @@ class Image(Resource):
     @token_required
     def post(self):
         """Get capture image."""
-        ca.raspiconfig.send("im")
-        return "", 204
+        try:
+            ca.raspiconfig.send("im")
+            return "", 204
+        except RaspiConfigError as error:
+            abort(422, error)        
 
 
 @api.response(204, "Action execute")
 @api.response(422, "Error", message)
 @api.response(403, "Forbidden", forbidden)
-@api.route("/captures/timelapse")
+@api.route("/captures/timelapse/start", endpoint="captures_timelapse_start")
+@api.route("/captures/timelapse/stop", endpoint="captures_timelapse_stop")
 class Timelapse(Resource):
     """Timelapse."""
 
     @token_required
-    @api.expect(action_m)
     def post(self):
         """Get capture Timelapse."""
-        if api.payload.get("action") == "start":
-            ca.raspiconfig.send("tl 1")
-        if api.payload.get("action") == "stop":
-            ca.raspiconfig.send("tl 0")
-        return "", 204
+        try:
+            if request.endpoint == "captures_timelapse_start":
+                ca.raspiconfig.send("tl 1")
+            if request.endpoint == "captures_timelapse_stop":
+                ca.raspiconfig.send("tl 0")
+            return "", 204
+        except RaspiConfigError as error:
+            abort(422, error)
+            
