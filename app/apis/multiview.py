6aa01b9034e4c673b5056c5c9507ff9157bc28a3
cyr-ius <cyr-ius@ipocus.net>
2023-10-15 13:53:15 +0200

    Rename stream to multiview

--- app/apis/multiview.py
+++ app/apis/multiview.py
@@ -3,64 +3,64 @@ from flask import current_app as ca
 from flask_restx import Namespace, Resource, abort
 
 from ..helpers.decorator import token_required
-from .models import message, stream, streams
+from .models import message, multiview, multiviews
 
 api = Namespace("multiview", path="/api")
 api.add_model("Error", message)
-api.add_model("Stream", stream)
-api.add_model("Streams", streams)
+api.add_model("Multiview", multiview)
+api.add_model("Multiviews", multiviews)
 
 
 @api.response(403, "Forbidden", message)
-@api.route("/multiview")
-class Streamers(Resource):
+@api.route("/multiviews")
+class Multiviews(Resource):
     """List hosts."""
 
     @token_required
-    @api.marshal_with(stream, as_list=True)
+    @api.marshal_with(multiview, as_list=True)
     def get(self):
         """List hosts."""
-        return ca.settings.get("streamers", [])
+        return ca.settings.get("multiviews", [])
 
     @token_required
-    @api.expect(streams, validate=True)
-    @api.marshal_with(stream)
+    @api.expect(multiview)
+    @api.marshal_with(multiviews)
     def post(self):
         """Create host."""
-        if ca.settings.get("streamers") is None:
-            ca.settings.streamers = []
-        ids = [streamer["id"] for streamer in ca.settings.streamers]
+        if ca.settings.get("multiviews") is None:
+            ca.settings.multiviews = []
+        ids = [multiview["id"] for multiview in ca.settings.multiviews]
         api.payload["id"] = 1 if len(ids) == 0 else max(ids) + 1
-        ca.settings.streamers.append(api.payload)
-        ca.settings.update(streamers=ca.settings.streamers)
+        ca.settings.multiviews.append(api.payload)
+        ca.settings.update(multiviews=ca.settings.multiviews)
         return api.payload
 
 
 @api.response(403, "Forbidden", message)
-@api.route("/multiview/<int:id>")
-class Streamer(Resource):
-    """Host objet."""
+@api.route("/multiviews/<int:id>")
+class Multiview(Resource):
+    """Multiview objet."""
 
     @token_required
-    @api.marshal_with(stream)
+    @api.marshal_with(multiview)
     @api.response(404, "Not found", message)
     def get(self, id: int):  # pylint: disable=W0622
-        """Get streamer."""
-        if stm := ca.settings.get_object("streamers", id):
+        """Get multiview."""
+        if stm := ca.settings.get_object("multiviews", id):
             return stm
         abort(404, "Host not found")
 
     @token_required
-    @api.expect(streams)
-    @api.marshal_with(stream)
+    @api.expect(multiviews)
+    @api.marshal_with(multiview)
     @api.response(404, "Not found", message)
     def put(self, id: int):  # pylint: disable=W0622
-        """Set streamer."""
+        """Set multiview."""
         api.payload["id"] = id
-        if stm := ca.settings.get_object("streamers", id):
-            ca.settings.streamers.remove(stm)
-            ca.settings.streamers.append(api.payload)
-            ca.settings.update(streamers=ca.settings.streamers)
+        if stm := ca.settings.get_object("multiviews", id):
+            ca.settings.multiviews.remove(stm)
+            ca.settings.multiviews.append(api.payload)
+            ca.settings.update(multiviews=ca.settings.multiviews)
             return api.payload
         abort(404, "Host not found")
 
@@ -68,9 +68,9 @@ class Streamer(Resource):
     @api.response(204, "Actions is success")
     @api.response(404, "Not found", message)
     def delete(self, id: int):  # pylint: disable=W0622
-        """Delete streamer."""
-        if stm := ca.settings.get_object("streamers", id):
-            ca.settings.streamers.remove(stm)
-            ca.settings.update(streamers=ca.settings.streamers)
+        """Delete multiview."""
+        if stm := ca.settings.get_object("multiviews", id):
+            ca.settings.multiviews.remove(stm)
+            ca.settings.update(multiviews=ca.settings.multiviews)
             return "", 204
         abort(404, "Host not found")
