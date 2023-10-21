a90c140dfd64d6ca8c43465c352df3946e7261e4
cyr-ius <cyr-ius@ipocus.net>
2023-10-21 11:35:14 +0200

    Call user class

--- app/apis/totp.py
+++ app/apis/totp.py
@@ -1,42 +1,13 @@
 """Blueprint API."""
-import pyotp
-import qrcode
-import qrcode.image.svg
-from flask import current_app as ca
-from flask_restx import Namespace, Resource, abort, fields
+from flask_restx import Namespace, Resource, abort
 
 from ..helpers.decorator import token_required
-from .models import message
+from ..helpers.users import User, UserNotFound
+from .models import message, otp
 
 api = Namespace("otps")
 api.add_model("Error", message)
-
-
-class UriOTP(fields.Raw):
-    """totp SVG."""
-
-    def output(self, key, obj, **kwargs):
-        if not obj:
-            return
-        name = obj["name"]
-        secret = obj["secret"]
-        uri = f"otpauth://totp/viewpicam:{name}?secret={secret}&issuer=viewpicam"
-        qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage)
-        qr.make(fit=True)
-        qr.add_data(uri)
-        img = qr.make_image()
-        return img.to_string(encoding="unicode")
-
-
-otp = api.model(
-    "TOTP",
-    {
-        "id": fields.Integer(required=True, description="Id"),
-        "name": fields.String(required=True, description="The user name"),
-        "otp_svg": UriOTP(required=False),
-        "totp": fields.Boolean(required=False),
-    },
-)
+api.add_model("TOTP", otp)
 
 
 @api.response(403, "Forbidden", message)
@@ -51,29 +22,27 @@ class Totp(Resource):
     @api.response(422, "Error", message)
     def get(self, id: int):  # pylint: disable=W0622
         """Get OTP for a user."""
-        if dict_user := ca.settings.get_object("users", id):
-            if dict_user.get("totp", False) is False:
-                ca.settings.users.remove(dict_user)
-                dict_user["secret"] = pyotp.random_base32()
-                ca.settings.users.append(dict_user)
-                if len(ca.settings.users) > 0:
-                    ca.settings.update(users=ca.settings.users)
-            return dict_user
-        abort(404, "User not found")
+        try:
+            user = User(id)
+            user.set_secret()
+            return user
+        except UserNotFound:
+            abort(404, "User not found")
 
     @token_required
     @api.response(204, "Action is success")
     @api.response(404, "Not found", message)
     def post(self, id: int):  # pylint: disable=W0622
         """Check OTP code."""
-        if dict_user := ca.settings.get_object("users", id):
-            if (secret := dict_user.get("secret")) and dict_user.get("totp", False):
-                totp = pyotp.TOTP(secret)
-                if totp.verify(api.payload["secret"]):
+        try:
+            user = User(id)
+            if user.totp is True:
+                if user.check_totp(api.payload["secret"]):
                     return "", 204
                 abort(422, "OTP incorrect")
             abort(422, "OTP not enable")
-        abort(404, "User not found")
+        except UserNotFound:
+            abort(404, "User not found")
 
     @token_required
     @api.response(204, "Action is success")
@@ -81,31 +50,22 @@ class Totp(Resource):
     @api.response(422, "Error", message)
     def put(self, id: int):  # pylint: disable=W0622
         """Check and create OTP Code for a user."""
-        if (dict_user := ca.settings.get_object("users", id)) and (
-            secret := dict_user.get("secret")
-        ):
-            totp = pyotp.TOTP(secret)
-            if totp.verify(api.payload["secret"]):
-                ca.settings.users.remove(dict_user)
-                dict_user["totp"] = True
-                ca.settings.users.append(dict_user)
-                if len(ca.settings.users) > 0:
-                    ca.settings.update(users=ca.settings.users)
+        try:
+            user = User(id)
+            if user.validate_secret(api.payload["secret"]):
                 return "", 204
             abort(422, "OTP incorrect")
-        abort(404, "User or otp code not found")
+        except UserNotFound:
+            abort(404, "User not found")
 
     @token_required
     @api.response(204, "Action is success")
     @api.response(404, "Not found", message)
     def delete(self, id: int):  # pylint: disable=W0622
         """Delete OTP infos for a user."""
-        if dict_user := ca.settings.get_object("users", id):
-            ca.settings.users.remove(dict_user)
-            dict_user.pop("totp", None)
-            dict_user.pop("secret", None)
-            ca.settings.users.append(dict_user)
-            if len(ca.settings.users) > 0:
-                ca.settings.update(users=ca.settings.users)
+        try:
+            user = User(id)
+            user.delete_secret()
             return "", 204
-        abort(404, "User not found")
+        except UserNotFound:
+            abort(404, "User not found")
