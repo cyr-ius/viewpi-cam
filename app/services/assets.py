8c327c2528ee52abdf390f9ead6178ba8442bc85
cyr-ius <cyr-ius@ipocus.net>
2023-10-22 16:53:11 +0200

    Remove bs_icon and add jinj2 filter

--- app/services/assets.py
+++ app/services/assets.py
@@ -1,30 +1,5 @@
-import re
-
-from flask_assets import Bundle, Filter
-from webassets.filter import get_filter
-
-
-class ConcatFilter(Filter):
-    """Filter that merges files, placing a semicolon between them.
-
-    Fixes issues caused by missing semicolons at end of JS assets, for example
-    with last statement of jquery.pjax.js.
-    """
-
-    def concat(self, out, hunks, **kw):
-        out.write(";".join([h.data() for h, info in hunks]))
-
-
-bs_icons = (
-    get_filter(
-        "cssrewrite",
-        replace=lambda url: re.sub(
-            r"./fonts/",
-            "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/fonts/",
-            url,
-        ),
-    ),
-)
+"""Use Flask-Asssets for build and minified css and js."""
+from flask_assets import Bundle
 
 css_main = Bundle(
     "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic",  # noqa
@@ -32,7 +7,7 @@ css_main = Bundle(
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
     Bundle(
         "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.min.css",  # noqa
-        filters=bs_icons,
+        filters="datauri",
     ),
     output="css/main.css",
 )
@@ -50,13 +25,16 @@ js_main = Bundle(
 )
 
 js_pipan = Bundle(
-    "../app/ressources/js/pipan.js", filters="rjsmin", output="js/pipan.js"
+    "../app/ressources/js/pipan.js", filters="jinja2,rjsmin", output="js/pipan.js"
+)
+
+js_colors = Bundle(
+    "../app/ressources/js/colorconverter.js",
+    "../app/ressources/js/color_modes.js",
+    filters="rjsmin",
+    output="js/colors.js",
 )
 
 js_custom = Bundle(
-    "../app/ressources/js/colorconverter.js",
-    "../app/ressources/js/color_modes.js",
-    "../app/ressources/js/custom.js",
-    filters="rjsmin",
-    output="js/custom.js",
+    "../app/ressources/js/custom.js", filters="jinja2,rjsmin", output="js/custom.js"
 )
