f295cb398495c880bd6e69f3af1b638831d69fde
Cyr-ius <1258123+cyr-ius@users.noreply.github.com>
2023-10-24 21:13:44 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-
-# Assets
-flask assets build
-
 # Add link media to static folder
 ln -s /app/media/ /app/static/
-
 exec gunicorn --bind 0.0.0.0:8000 --workers 1 --worker-class gthread --threads 4 'app:create_app()' "$@"
