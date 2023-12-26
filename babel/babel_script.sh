eeb78aa8236076525723ca92180440bc3c3a4cdf
cyr-ius <cyr-ius@ipocus.net>
2023-12-26 19:10:14 +0100

    Update

--- babel/babel_script.sh
+++ babel/babel_script.sh
@@ -1,1 +1,3 @@
-pybabel extract -F babel.cfg -k lazy_gettext --project=viewpicam  -o ./messages.pot ../app && pybabel update -i ./messages.pot -d ../app/translations && pybabel compile -d ../app/translations
+pybabel extract -F babel.cfg -k lazy_gettext --project=viewpicam -o ./messages.pot ../app 
+pybabel update -i ./messages.pot -d ../app/translations
+pybabel compile -d ../app/translations
