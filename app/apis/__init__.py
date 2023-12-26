dfb5ae582cb488676092f0954cef7def7dc540d2
cyr-ius <cyr-ius@ipocus.net>
2023-12-26 19:13:32 +0100

    Update namespace

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -2,15 +2,15 @@
 from flask import Blueprint
 from flask_restx import Api
 
-from .captures import api as ns3
-from .logs import api as ns2
-from .multiview import api as ns7
-from .previews import api as ns4
-from .schedule import api as ns5
-from .settings import api as ns1
-from .system import api as ns6
-from .totp import api as ns8
-from .users import api as ns9
+from .captures import api as captures
+from .logs import api as logs
+from .multiview import api as multiview
+from .previews import api as previews
+from .schedule import api as schedule
+from .settings import api as settings
+from .system import api as system
+from .totp import api as totp
+from .users import api as users
 
 bp = Blueprint("api", __name__)
 
@@ -25,12 +25,12 @@ api = Api(
     },
     security="apikey",
 )
-api.add_namespace(ns1)
-api.add_namespace(ns2)
-api.add_namespace(ns3)
-api.add_namespace(ns4)
-api.add_namespace(ns5)
-api.add_namespace(ns6)
-api.add_namespace(ns7)
-api.add_namespace(ns8)
-api.add_namespace(ns9)
+api.add_namespace(captures)
+api.add_namespace(logs)
+api.add_namespace(multiview)
+api.add_namespace(previews)
+api.add_namespace(schedule)
+api.add_namespace(settings)
+api.add_namespace(system)
+api.add_namespace(totp)
+api.add_namespace(users)
