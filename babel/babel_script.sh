8aec3e5e05eb20be4c42a1f3e81fce2e91204fc5
cyr-ius <cyr-ius@ipocus.net>
2023-12-26 18:43:03 +0100

    Move to babel folder

---
+++ babel/babel_script.sh
@@ -0,0 +1,1 @@
+pybabel extract -F babel.cfg -k lazy_gettext --project=viewpicam  -o ./messages.pot ../app && pybabel update -i ./messages.pot -d ../app/translations && pybabel compile -d ../app/translations
--- babel_script.sh
+++
@@ -1,1 +0,0 @@
-pybabel extract -F babel.cfg -k lazy_gettext --project=viewpicam  -o app/messages.pot . && pybabel update -i app/messages.pot -d app/translations && pybabel compile -d app/translations
