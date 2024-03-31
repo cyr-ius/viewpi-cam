d8b4885ed6bf9239f327812cf760ecce7806f866
cyr-ius <cyr-ius@ipocus.net>
2024-03-31 17:48:33 +0200

    Rename variables

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,19 +5,18 @@ cd /app
 # Add link media to static folder
 ln -s -f /app/media/ /app/static/
 
-GUNICORN_TIMEOUT="${GUNICORN_TIMEOUT:-120}"
-GUNICORN_WORKERS="${GUNICORN_WORKERS:-1}"
-GUNICORN_LOGLEVEL="${GUNICORN_LOGLEVEL:-info}"
+TIMEOUT="${TIMEOUT:-120}"
+WORKERS="${WORKERS:-1}"
+LOGLEVEL="${LOGLEVEL:-info}"
 BIND_ADDRESS="${BIND_ADDRESS:-0.0.0.0:8000}"
 WORKER_CLASS="${WORKER_CLASS:-gthread}"
 THREADS="${THREADS:-4}"
 
-GUNICORN_ARGS="-t ${GUNICORN_TIMEOUT} --workers ${GUNICORN_WORKERS} --bind ${BIND_ADDRESS} --log-level ${GUNICORN_LOGLEVEL} --worker-class ${WORKER_CLASS} --threads ${THREADS}"
+GUNICORN_ARGS="-t ${TIMEOUT} --workers ${WORKERS} --bind ${BIND_ADDRESS} --log-level ${LOGLEVEL} --worker-class ${WORKER_CLASS} --threads ${THREADS}"
 
 if [ "$1" == gunicorn ]; then
     /bin/sh -c "flask db upgrade"
     exec "$@" $GUNICORN_ARGS
-
 else
     exec "$@"
 fi
