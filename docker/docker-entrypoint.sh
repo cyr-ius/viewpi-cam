7b80f1a9c0e582f4cd95f2c59e9212aa91fa3e0b
cyr-ius <cyr-ius@ipocus.net>
2024-04-01 12:47:58 +0200

    Rename loglevel

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,12 +7,12 @@ ln -s -f /app/media/ /app/static/
 
 TIMEOUT="${TIMEOUT:-120}"
 WORKERS="${WORKERS:-1}"
-LOGLEVEL="${LOGLEVEL:-info}"
+LOG_LEVEL="${LOG_LEVEL:-info}"
 BIND_ADDRESS="${BIND_ADDRESS:-0.0.0.0:8000}"
 WORKER_CLASS="${WORKER_CLASS:-gthread}"
 THREADS="${THREADS:-4}"
 
-GUNICORN_ARGS="-t ${TIMEOUT} --workers ${WORKERS} --bind ${BIND_ADDRESS} --log-level ${LOGLEVEL} --worker-class ${WORKER_CLASS} --threads ${THREADS}"
+GUNICORN_ARGS="-t ${TIMEOUT} --workers ${WORKERS} --bind ${BIND_ADDRESS} --log-level ${LOG_LEVEL} --worker-class ${WORKER_CLASS} --threads ${THREADS}"
 
 if [ "$1" == gunicorn ]; then
     /bin/sh -c "flask db upgrade"
