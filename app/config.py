be51d6c2ef2920f6136caa9fb9a4ea3990f4d28b
cyr-ius <cyr-ius@ipocus.net>
2024-04-01 13:26:33 +0200

    Rename media to media_folder

--- app/config.py
+++ app/config.py
@@ -2,12 +2,9 @@
 
 import os
 
-basedir = os.path.abspath(os.path.abspath(os.path.dirname(__file__)))
-
 # GENERAL SETTINGS
 SITE_NAME = "ViewPI Camera"
 VERSION = os.getenv("VERSION", "0.0.0")
-SYSTEM_FOLDER = f"{basedir}/../config"
 
 # BASIC APP CONFIG
 SECRET_KEY = os.getenv("SECRET_KEY", "12345678900987654321")
@@ -21,35 +18,43 @@ SESSION_COOKIE_SAMESITE = "Lax"
 # Allowed extension for mask file
 ALLOWED_EXTENSIONS = ["pgm"]
 MASK_FILENAME = "motionmask.pgm"
+
 # The host running the application
 HOSTNAME = os.uname()[1]
+
 # Url and timeout to fetch version from github
 GIT_URL = "https://api.github.com/repos/cyr-ius/viewpi-cam/releases/latest"
 TIMEOUT = 10
+
 # Name of this camera
 CAM_NAME = "mycam"
+
 # Unique camera string build from application name, camera name, host name
 CAM_STRING = f"{SITE_NAME} {VERSION}: {CAM_NAME}@{HOSTNAME}"
+
 # File where default settings
 RASPI_CONFIG = "/etc/raspimjpeg"
 RASPI_BINARY = "/usr/bin/raspimjpeg"
-# File where user specific settings changes are stored
-MEDIA = "media"
+MEDIA_FOLDER = "media"
+
 # Character used to flatten file paths
 THUMBNAIL_EXT = ".th.jpg"
+
 # Select size metho 0=python , 1=stats
 FILESIZE_METHOD = 0
+
 # File for settings
-SERVO_FILE = f"{SYSTEM_FOLDER}/servo"
-PIPAN_FILE = f"{SYSTEM_FOLDER}/pipan"
+SERVO_FILE = "servo"
+PIPAN_FILE = "pipan"
+
 # Convert command
 CONVERT_CMD = "/usr/bin/ffmpeg -f image2 -i i_%05d.jpg"
+
 # Userlevel
 USERLEVEL_MIN = 1
 USERLEVEL_MINP = 2
 USERLEVEL_MEDIUM = 4
 USERLEVEL_MAX = 8
-
 USERLEVEL = {
     "min": USERLEVEL_MIN,
     "preview": USERLEVEL_MINP,
@@ -59,6 +64,7 @@ USERLEVEL = {
 
 # Locales
 LOCALES = ["en", "fr"]
+
 # Macros name files
 MACROS = [
     "error_soft",
@@ -72,9 +78,9 @@ MACROS = [
     "motion_event",
     "startstop",
 ]
+
 # Filter number
 TIME_FILTER_MAX = 8
-
 
 # Starts services
 SVC_RASPIMJPEG = os.getenv("SVC_RASPIMJPEG", "1")
