43f632c4dd743d72967c5d0895f0dfdbf56ad5a7
cyr-ius <cyr-ius@ipocus.net>
2024-07-13 16:05:36 +0200

    Fix if exist real file

--- app/helpers/database.py
+++ app/helpers/database.py
@@ -2,7 +2,14 @@
 
 from flask import current_app as ca
 
-from ..helpers.filer import get_file_id, get_file_info, is_thumbnail, list_folder_files
+from ..helpers.filer import (
+    data_file_name,
+    get_file_id,
+    get_file_info,
+    is_existing,
+    is_thumbnail,
+    list_folder_files,
+)
 from ..helpers.utils import write_log
 from ..models import Files as files_db
 from ..models import db
@@ -13,7 +20,11 @@ def update_img_db() -> None:
     media_path = ca.raspiconfig.media_path
     files = db.session.execute(db.Select(files_db.id)).scalars().all()
     for thumb in list_folder_files(media_path):
-        if is_thumbnail(thumb) and (get_file_id(thumb) not in files):
+        if (
+            is_thumbnail(thumb)
+            and (get_file_id(thumb) not in files)
+            and is_existing(data_file_name(thumb))
+        ):
             info = get_file_info(thumb)
             file = files_db(**info)
             files.append(file.id)
